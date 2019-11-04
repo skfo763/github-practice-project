04bada58f28e7fb36d6579bfa13b96fd2b84c020
Changyeon-Seo <cy.seo@mozzet.com>
2019-11-04 18:36:55 +0900

    no message

--- sample-github-rx/src/main/java/com/example/sample_github_rx/SampleGithubRxApp.kt
+++ sample-github-rx/src/main/java/com/example/sample_github_rx/SampleGithubRxApp.kt
@@ -4,7 +4,7 @@ import com.example.sample_github_rx.dagger.DaggerAppComponent
 import dagger.android.AndroidInjector
 import dagger.android.DaggerApplication
 
-class SampleGithubRxApp:DaggerApplication() {
+class SampleGithubRxApp: DaggerApplication() {
     override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
         return DaggerAppComponent.builder().application(this).build()
     }
